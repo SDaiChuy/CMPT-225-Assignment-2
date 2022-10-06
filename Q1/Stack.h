f9d375aba8ca15f7ce50ad49599fd35b24434312
Steven Dai Chuy <stevendaichuy1@d142-058-035-147.wireless.sfu.ca>
2022-10-05 18:22:44 -0700

    Work on Q1

--- Q1/Stack.h
+++ Q1/Stack.h
@@ -5,8 +5,8 @@
  * Class Invariant: LIFO order
  *                  Top of Stack located at the back of SHSL list.
  *
- * Author:
- * Date:
+ * Author: Steven Dai Chuy
+ * Date: October 5, 2022
  */
  
 class Stack {
@@ -18,14 +18,48 @@ class Stack {
             public:
                 int data;
                 StackNode * next;
+                unsigned int elementCount;
+                
         };
-        
+
+    StackNode* head; 
 /* Put your code here! */       
         
 
     public:
     
 /* Put your code here! */   
+
+//constructors
+Stack();
+
+//Destructor
+~Stack();
+
+
+//getters
+int getData() const;
+
+unsigned int getElementCount() const;
+
+//setters;
+void setData(int data);
+
+//methods
+
+// check to see if the stack is empty, return NULL if empty
+bool isEmpty() const;
+
+// push an element onto the top of the stack
+bool push(int element);
+
+// returns the values of the top element
+int peek() const;
+
+// Removes the top element of the stack
+bool pop();
+
+
 
 
 };
